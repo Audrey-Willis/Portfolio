23cef833b1afc93603a83bc5dfb947ec01d40df8
Audrey-Willis <nnikky15@gmail.com>
2021-03-01 00:40:06 -0500

    added new component to router

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
+import LandingPage from "./components/landingPage/LandingPage.js";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
   <BrowserRouter>
-    <Route exact path = "/"component = {Header}/>
+  <Header/>
+    <Route exact path = "/"component = {LandingPage}/>
     <Route path = "/About" component = {About}/>
     <Route path = "/Experience" component = {Experience}/>
     <Route Path = "/Projects" component = {Projects}/>
     <Route path = "/Resume" component = {Resume}/>
+    <Route path = "/Contact" component = {Footer}/>
   </BrowserRouter>
   );
 }
