807404549ddd95b600f02f02624b1a8f488343df
Audrey-Willis <nnikky15@gmail.com>
2021-03-01 00:33:50 -0500

    set uo routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router,Route, BrowserRouter} from "react-router-dom";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Projects from "./components/Projects/Projects";
+import Resume from "./components/Resume/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+    <Route exact path = "/"component = {Header}/>
+    <Route path = "/About" component = {About}/>
+    <Route path = "/Experience" component = {Experience}/>
+    <Route Path = "/Projects" component = {Projects}/>
+    <Route path = "/Resume" component = {Resume}/>
+  </BrowserRouter>
   );
 }
 
