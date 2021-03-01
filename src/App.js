24bac4131d9e4d2e468a40a99ab24578639536cc
Audrey-Willis <nnikky15@gmail.com>
2021-03-01 13:02:03 -0500

    fixed issue with projects component being fixed to bottom of app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {BrowserRouter as Router,Route, BrowserRouter} from "react-router-dom";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
-import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/Resume";
 import TitlePage from "./components/Title-Page/TitlePage";
 import Footer from "./components/Footer/Footer";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
     <Route exact path = "/"component = {TitlePage}/>
     <Route path = "/About" component = {About}/>
     <Route path = "/Experience" component = {Experience}/>
-    <Route Path = "/Projects" component = {Projects}/>
     <Route path = "/Resume" component = {Resume}/>
     <Route path = "/Contact" component = {Footer}/>
+    <Route path = "/Projects" component = {Projects}/>;
   </BrowserRouter>
   );
 }
