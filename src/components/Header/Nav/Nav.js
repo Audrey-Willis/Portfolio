6677f43020f40766a9748bed4302d8f2bb326ead
Audrey-Willis <nnikky15@gmail.com>
2021-03-01 15:41:48 -0500

    added image back to title page

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -8,10 +8,7 @@ function Nav(){
             <Link to = "/">Title-Page</Link>
             <Link to = "/About">ABout-Me</Link>
             <Link to = "/Experience">Experience</Link>
-            <div>
-                <img src="https://i.ibb.co/JdcPtQW/49899534-10217875493900119-6053761798190923776-n.jpg" alt = "Audrey"/>
             <h1>Audrey</h1>
-            </div>
             <Link to = "/Projects">Projects</Link>
             <Link to = "/Resume">Resume</Link>
             <Link to = "./Contact">Contact</Link>
--- src/components/Title-Page/TitlePage.js
+++ src/components/Title-Page/TitlePage.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function TitlePage(){
-    return(
-        <div>
+    return (
+      <div>
         <h1>Audrey Willis</h1>
         <h2>Front-End Developer</h2>
-        <h3>East TN</h3>
-        </div>
-    )
+        <h3>East TN</h3>{" "}
+        <img
+          src="https://i.ibb.co/JdcPtQW/
+49899534-10217875493900119-6053761798190923776-n.jpg"
+          alt="Audrey"
+        />
+      </div>
+    );
 }
 export default TitlePage;
