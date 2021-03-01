d8987011637e609c11a221b3a407da40f1734f40
Audrey-Willis <nnikky15@gmail.com>
2021-03-01 15:20:59 -0500

    erased photo from title page

--- src/components/Title-Page/TitlePage.js
+++ src/components/Title-Page/TitlePage.js
@@ -6,7 +6,6 @@ function TitlePage(){
         <h1>Audrey Willis</h1>
         <h2>Front-End Developer</h2>
         <h3>East TN</h3>
-        <img src="https://i.ibb.co/x8yRwqF/78461837-10220645291823336-2269647485859266560-o.jpg" alt = "Audrey"/>
         </div>
     )
 }
