fe29ac216f48e55f813d8cd2e1ef4750db4e59cd
Audrey-Willis <nnikky15@gmail.com>
2021-03-01 15:32:13 -0500

    added image to nav

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,17 +1,21 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {NavStyle} from "./NavStyle";
 
 function Nav(){
     return(
-        <nav>
+        <NavStyle>
             <Link to = "/">Title-Page</Link>
             <Link to = "/About">ABout-Me</Link>
             <Link to = "/Experience">Experience</Link>
+            <div>
+                <img src="https://i.ibb.co/JdcPtQW/49899534-10217875493900119-6053761798190923776-n.jpg" alt = "Audrey"/>
             <h1>Audrey</h1>
+            </div>
             <Link to = "/Projects">Projects</Link>
             <Link to = "/Resume">Resume</Link>
             <Link to = "./Contact">Contact</Link>
-         </nav>
+         </NavStyle>
     )
 }
 export default Nav;
