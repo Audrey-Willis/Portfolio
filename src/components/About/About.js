58757a9ec21985d3e43c0013f1a3f924a20a333d
Audrey-Willis <nnikky15@gmail.com>
2021-03-03 17:12:00 -0500

    decided to change about before styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,27 +1,11 @@
 import React from "react";
+import {AboutStyle} from "./AboutStyle";
+
 
 function About(){
-    return(
-        <div>
-            <div className = "about">
-        <h1>Welcome!</h1>
-        <p>Drum Roll Please</p>
-        <p>Que the applause</p>
-        <p>My name is Audrey Willis. I'm a semi-young woman living in eastern Tennessee with my fiance and three great danes.</p>
-        <p>I love challenges and puzzles. My favorite feeling in the world is the sense of accomplishment one feels when they've been struggling with a problem and have finally gotten to their eureka moment. </p>
-        <p>More than anything, I'm a lover of life-long learning. I don't consider it a day well spent unless I've expanded my mind in some way. I'm eager to continue to learn coding and can't wait to see my skills improve with time and practice. </p>
-        
-        <div>
-            <h2>Why I love web development</h2>
-            <p>The world is fast growing and ever changing. In no time at all we've woven technology and the web so much into the fabric of our society, that it's an integral part of how we now connect and share as humans. Theres a sense of pride that comes from knowing that one can contribute to the network of information, both valuable and mundane, that holds us all together</p>
-        </div>
-        <div>
-            <h2>My Future Goals</h2>
-            <p> I'd like to learn more about graphic design and animation so that I can use my skills to design,build, and manage a website that makes free educational cartoons for kids on topics ranging from science and coding, to mythology and astronomy.</p>
-        </div>
-        </div>
-        </div>
-        
-    )
-}
+   return (
+      <AboutStyle>
+        <h1>About</h1>
+      </AboutStyle>
+    )}
 export default About;
