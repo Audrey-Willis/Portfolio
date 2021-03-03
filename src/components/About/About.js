2e655a5bdfdc753040bbd2297bfe1c6d0e3ae587
Audrey-Willis <nnikky15@gmail.com>
2021-03-03 18:59:58 -0500

    styled about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,36 @@
 import React from "react";
 import {AboutStyle} from "./AboutStyle";
+import{Link} from "react-router-dom"
 
 
-function About(
-    return(
-        <div>
-            <div className = "about">
-              <div className = "about-header">
-        <p>Drum Roll Please</p>
-        <p>Cue the applause</p>
+function About(){
+    return (
+      <AboutStyle>
+        <div className="about rotate-in-center">
+          <div className="about-header">
+            <p className="wobble-hor-bottom">🎉Drum Roll Please🎉</p>
+            <p className="wobble-hor-bottom">🎉Cue the applause🎉</p>
+          </div>
+          <div className="about-p">
+            <h1>Welcome!</h1>
+            <p>
+              My name is Audrey Willis. I'm a semi-young woman living in eastern
+              Tennessee with my fiance and three great danes. I love challenges
+              and puzzles. My favorite feeling in the world is the sense of
+              accomplishment one feels when they've been struggling with a
+              problem and have finally gotten to their eureka moment.{" "}
+            </p>
+            <p>
+              More than anything, I'm a lover of life-long learning. I don't
+              consider it a day well spent unless I've expanded my mind in some
+              way. I'm eager to benefit from the knowledge of more senior
+              developers in the workplace. I hope to always be better than I was
+              the day before.{" "}
+            </p>
+          </div>
+          <Link to="/Experience" className = "arrow">&#8595;</Link>
         </div>
-        <p>My name is Audrey Willis. I'm a semi-young woman living in eastern Tennessee with my fiance and three great danes.</p>
-        <p>I love challenges and puzzles. My favorite feeling in the world is the sense of accomplishment one feels when they've been struggling with a problem and have finally gotten to their eureka moment. </p>
-        <p>More than anything, I'm a lover of life-long learning. I don't consider it a day well spent unless I've expanded my mind in some way. I'm eager to benefit from the knowledge of more senior developers in the workplace. I hope to always be better than I was the day before. </p>
-        </div>
-        </div>
-        
-    )
+      </AboutStyle>
+    );
 }
 export default About;
--- src/components/About/AboutStyle.js
+++ src/components/About/AboutStyle.js
@@ -3,5 +3,96 @@ import styled from "styled-components";
 export const AboutStyle = styled.div`
   background-color: #e0bbe4;
   font-family: "Gloria Hallelujah", cursive;
-  background-image: url("https://i.ibb.co/r6V3jLc/shutterstock-724785052.png");
-  `
+  padding: 2%;
+  .about {
+    background-image: url("https://i.ibb.co/r6V3jLc/shutterstock-724785052.png");
+    padding: 2%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border: 5px double black;
+  }
+  .about-p {
+    width: 70%;
+    background-color: #e0bbe4;
+    border: 5px double black;
+    font-size: 0.9rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .about-p p {
+    background-color: pink;
+    width: 90%;
+    padding: 2%;
+  }
+  .about-p h1 {
+    text-align: center;
+  }
+  .about-header {
+    display: flex;
+    justify-content: space-around;
+    width: 90%;
+  }
+  .about-header p {
+    background-color: pink;
+  }
+  .arrow {
+    font-size: 3rem;
+    text-decoration: none;
+    color: black;
+  }
+  .arrow:hover {
+    transform: (1.1);
+  }
+  .arrow:active {
+    color: black;
+  }
+  .wobble-hor-bottom {
+    animation: wobble-hor-bottom 1.5s infinite both;
+    padding: 1%;
+    border: 2px double black;
+    border-radius: 50%;
+  }
+  .rotate-in-center {
+    -webkit-animation: rotate-in-center 1s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+      both;
+    animation: rotate-in-center 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+  }
+  @keyframes wobble-hor-bottom {
+    0%,
+    100% {
+      transform: translateX(0%);
+      transform-origin: 50% 50%;
+    }
+    15% {
+      transform: translateX(-30px) rotate(-6deg);
+    }
+    30% {
+      transform: translateX(15px) rotate(6deg);
+      background-color: lightblue;
+    }
+    45% {
+      transform: translateX(-15px) rotate(-3.6deg);
+    }
+    60% {
+      transform: translateX(9px) rotate(2.4deg);
+    }
+    75% {
+      transform: translateX(-6px) rotate(-1.2deg);
+    }
+  }
+  @keyframes rotate-in-center {
+    0% {
+      -webkit-transform: rotate(-360deg);
+      transform: rotate(-360deg);
+      opacity: 0;
+    }
+    100% {
+      -webkit-transform: rotate(0);
+      transform: rotate(0);
+      opacity: 1;
+    }
+  }
+`;
