56404935d3c16d5cb78b31079770e01783e0a16e
Audrey-Willis <nnikky15@gmail.com>
2021-03-06 18:39:12 -0500

    started on projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Projects(){
     return(
-        <h1>Projects</h1>
+        <div>
+          <div className = "projects">
+          <hi>Projects</hi>
+          <div className = "essentialisim-project">
+            <h2>Essentialisim</h2>
+          </div>
+          </div>
+        </div>
     )
 }
 export default Projects;
