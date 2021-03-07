30d312ba467282bd0ea315e423dbab230a584cd0
Audrey-Willis <nnikky15@gmail.com>
2021-03-07 14:56:56 -0500

    added gifs of projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,10 @@ function Projects(){
           <h1>Projects</h1>
           <div className="essentialisim-project">
             <h2>Essentialisim</h2>
+            <img
+              src="https://i.ibb.co/r0BtncQ/ezgif-com-gif-maker.gif"
+              alt="site video"
+            />
             <a href="#">Live Site</a>
             <a href="#">Github Repo</a>
             <h3>About:</h3>
@@ -18,6 +22,10 @@ function Projects(){
           </div>
           <div className="react-plants-project">
             <h2>React Plants</h2>
+            <img
+              src="https://i.ibb.co/X3SYqgK/ezgif-com-gif-maker-3.gif"
+              alt="site video"
+            />
             <a href="#">Live Site</a>
             <a href="#">Github Repo</a>
             <h3>About:</h3>
@@ -29,6 +37,10 @@ function Projects(){
           </div>
           <div className="rick-and-morty-project">
             <h2>Rick and Morty</h2>
+            <img
+              src="https://i.ibb.co/JQ86w01/ezgif-com-gif-maker-2.gif"
+              alt="site video"
+            />
             <a href="#">Live Site</a>
             <a href="#">Github Repo</a>
             <h3>About:</h3>
@@ -40,6 +52,10 @@ function Projects(){
           </div>
           <div className="nasa-potd-project">
             <h2>Nasa Photo of the Day</h2>
+            <img
+              src="https://i.ibb.co/nwtmSWD/ezgif-com-gif-maker-1.gif"
+              alt="site video"
+            />
             <a href="#">Live Site</a>
             <a href="#">Github Repo</a>
             <h3>About:</h3>
@@ -51,6 +67,10 @@ function Projects(){
           </div>
           <div className="car-customization-project">
             <h2>Car Customization Cart</h2>
+            <img
+              src="https://i.ibb.co/ggDXpw7/ezgif-com-gif-maker.gif"
+              alt="site-video"
+            />
             <a href="#">Live Site</a>
             <a href="#">Github Repo</a>
             <h3>About:</h3>
@@ -62,6 +82,10 @@ function Projects(){
           </div>
           <div className="redux-to-do-project">
             <h2>Redux To-Do List</h2>
+            <img
+              src="https://i.ibb.co/SNK8gzc/ezgif-com-gif-maker-2.gif"
+              alt="site-video"
+            />
             <a href="#">Live Site</a>
             <a href="#">Github Repo</a>
             <h3>About:</h3>
