cce68d7f9dcfc77a80fd8668568bffa4585cde10
Audrey-Willis <nnikky15@gmail.com>
2021-03-14 22:42:18 -0400

    added resume img to resume component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Resume(){
     return(
-        <h1>Resume</h1>
+        <div>
+            <h1>Resume</h1>
+            <img src = "https://i.ibb.co/8rH8TSR/Cream-and-Gray-Modern-Resume.jpg" alt = "resume"/>
+        </div>
     )
 }
 export default Resume;
