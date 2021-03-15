8082a26ee74fbd0c06c46057c7fcbfb3bfb081ce
Audrey-Willis <nnikky15@gmail.com>
2021-03-15 02:51:52 -0400

    made resume image into link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,8 +3,12 @@ import React from "react";
 function Resume(){
     return(
         <div>
-            <h1>Resume</h1>
-            <img src = "https://i.ibb.co/8rH8TSR/Cream-and-Gray-Modern-Resume.jpg" alt = "resume"/>
+            <div className = "resume-main">
+            <h1>My Resume</h1>
+            <h2>Click to open PDF file.</h2>
+           <a href = "https://drive.google.com/file/d/1vJEdGCGKteEKtHPxpclVQvERXEdxiDKE/view?usp=sharing"> <img src = "https://i.ibb.co/8rH8TSR/Cream-and-Gray-Modern-Resume.jpg" alt = "resume"/>
+           </a>
+           </div>
         </div>
     )
 }
