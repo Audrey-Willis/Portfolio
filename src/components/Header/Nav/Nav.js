84865004467e8caee25a38e04f50581fefdc285d
Audrey-Willis <nnikky15@gmail.com>
2021-04-30 17:13:37 -0400

    put in a new resume

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -7,7 +7,7 @@ function Nav(){
         <a aria-label = "about-me" href="#about">About Me</a>
         <a aria-label = "experience" href="#experience">Experience </a>
         <a aria-label = "projects" href="#projects">Projects</a>
-        <a aria-label = "resume" href="https://drive.google.com/file/d/1vJEdGCGKteEKtHPxpclVQvERXEdxiDKE/view?usp=sharing">Resume </a>
+        <a aria-label = "resume" href="https://drive.google.com/file/d/1bPhW-E49b6FKSFYARwCGWQf3T6vo6hO_/view?usp=sharing">Resume </a>
         <a aria-label = "contact" href="#contact">Contact</a>
       </NavStyle>
     );
